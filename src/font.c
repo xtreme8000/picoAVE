151584d5bb094434babef10e6965c8851e75069a
xtreme8000 <xtreme8000@users.noreply.github.com>
2024-08-21 15:40:05 +0200

    Add GPLv3 license

--- src/font.c
+++ src/font.c
@@ -1,3 +1,22 @@
+/*
+	Copyright (c) 2024 xtreme8000
+
+	This file is part of picoAVE.
+
+	picoAVE is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	picoAVE is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with picoAVE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <string.h>
 
 #include "font.h"
@@ -7,6 +26,7 @@
 #define FONT_STRIDE (128 - FONT_SKIPPED)
 
 // Spleen 8x16, https://github.com/fcambus/spleen
+// BSD 2-Clause license
 static uint8_t font[FONT_CHAR_HEIGHT * FONT_STRIDE] = {
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
